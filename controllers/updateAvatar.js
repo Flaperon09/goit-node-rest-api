504ab320421f6cc7b9198e6cb44cde188c70a1fe
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-08-13 14:02:13 +0300

    Выполнено ДЗ 5 Node

--- controllers/updateAvatar.js
+++ controllers/updateAvatar.js
@@ -2,7 +2,7 @@ import User from "../models/user.js";
 import { join } from "path"; // Пакет работы с путями к файлам
 import fs from "fs/promises"; // Пакет работы с файлами
 import { error } from "console";
-import Jimp from "jimp"; // Пакет обработки аватара
+import resizeImage from "../helpers/resizeImage.js";
 
 // Путь к папке avatars
 const __dirname = import.meta.dirname;
@@ -18,24 +18,36 @@ const updateAvatar = async (req, res) => {
     // Получение пути к временной папке и оригинального имени файла
     const { path: tempUpload, originalname } = req.file;
 
-    const { _id: id } = req.user; // Получение id пользователя
-    const avatarName = `${id}_${originalname}`; // Имя аватара с уникальным id
+    // Получение id пользователя
+    const { _id: id } = req.user; 
+
+    // Обработка изображения с помощью JIMP
+    await resizeImage(tempUpload);
+
+    // Получение имени аватара с уникальным id
+    const avatarName = `${id}_${originalname}`; 
     
     try {
-
+        // Путь к файлу аватара в папке /public/avatars
         const resultUpload = join(avatarsDir, avatarName);
 
         // Перемещение файла из папки tempUpload в папку resultUpload
         await fs.rename(tempUpload, resultUpload);
 
+        // Получение URL аватара
         const avatarURL = join("public", "avatars", avatarName);
+
         // Сохранение нового аватара
         await User.findByIdAndUpdate(req.user._id, { avatarURL });
+
+        // Возврат ответа с URL аватара
         res.json({ avatarURL });
+
     } catch (error) {
         // Если файл не перемещён - удалить его из временной папки
         await fs.unlink(tempUpload);
-        throw error;
+        // throw error;
+        throw HttpError(401);
     }
 };
  export default updateAvatar
